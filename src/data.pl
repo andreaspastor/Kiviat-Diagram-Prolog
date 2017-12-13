6c1b604a97b17dffced1f686575bc2c0be91b942
Antonin Arquey <arquey.antonin@gmail.com>
2017-12-13 16:05:52 +0100

    Ajout de predicat stockant les données et calculs des aires pour chaque permutations des diagrammes

---
+++ src/data.pl
@@ -0,0 +1,19 @@
+max(dim1, 4).
+max(dim2, 5).
+max(dim3, 8).
+max(dim4, 5).
+
+value(diag1, dim1, 4).
+value(diag1, dim2, 1).
+value(diag1, dim3, 2).
+value(diag1, dim4, 2).
+
+value(diag2, dim1, 3).
+value(diag2, dim2, 5).
+value(diag2, dim3, 8).
+value(diag2, dim4, 3).
+
+value(diag3, dim1, 3).
+value(diag3, dim2, 2).
+value(diag3, dim3, 3).
+value(diag3, dim4, 5).
--- src/main.pl
+++ src/main.pl
@@ -1,44 +1,14 @@
 :- include('./aire.pl').
 :- include('./list_utils.pl').
-
-/*maxListeReturn(X, I) :-
-  maxListe(X, 0, 0, 0, I).
-
-maxListe([], _, _, IndiceMax, I) :-
-  I is IndiceMax.
-
-maxListe([A | X], 0, 0, 0, I) :-
-  format('MaxListe 0 : ~w\n', A),
-  maxListe(X, A, 0, 0, I).
-
-maxListe([A | X], Max, Indice, IndiceMax, I) :-
-  A <= Max,
-  format('Je suis inferieure\n'),
-  NewIndice is Indice+1,
-  format('AireMax : ~w\n', Max),
-  maxListe(X, Max, NewIndice, IndiceMax, I).
-
-maxListe([A | X], Max, Indice, IndiceMax, I) :-
-  A > Max,
-  format('Je suis superieure\n'),
-  NewIndice is Indice+1,
-  NewIndiceMax is NewIndice,
-  format('AireMax : ~w\n', A),
-  maxListe(X, A, NewIndice, NewIndiceMax, I).
-*/
-
-
-
+:- include('./data.pl').
 
 :- initialization main.
 main :-
-  findall(Perm, permutation([(1, 1), (1, 8), (1,1), (1,8)], Perm), Perms),
-  computeAllAreas(Perms, Rep),
-  max_list(Rep, Max, I),
-  getListe(Perms, I, BestPerm),
-  format('Best permutation : '),
-  writeln(BestPerm),
-  format('Max aire : '),
-  writeln(Max),
+  ListeDim=[dim1, dim2, dim3, dim4],
+  ListeDiag=[diag1, diag2, diag3],
+  findall((S, X), ( permutation(ListeDim, X) ,
+    findall(AireValue, ( member(Diag, ListeDiag), aire(Diag, X, AireValue) ), S)
+  ), Areas),
+  writeln(Areas),
   halt.
 main :- halt(1).
